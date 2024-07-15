1859ea655cb274368ad103f4aad1c1155fa9b566
Shelwin <shelwinsunga@gmail.com>
2024-07-14 21:10:26 -0400

    feat: print method and operand in constructor

--- src/main.zig
+++ src/main.zig
@@ -5,29 +5,38 @@ const info = std.log.info;
 const Node = struct {
     value: f32,
     grad: f32,
+    operand: ?u8,
 
-    pub fn init(value: f32, grad: f32) Node {
+    pub fn init(value: f32, grad: f32, operand: ?u8) Node {
         return Node{
             .value = value,
             .grad = grad,
+            .operand = operand,
         };
     }
 
     pub fn add(self: Node, other: Node) Node {
-        return Node{
-            .value = self.value + other.value,
-            .grad = self.grad + other.grad,
-        };
+        const output = Node.init(self.value + other.value, 0.0, '+');
+        return output;
+    }
+
+    pub fn print(self: Node) void {
+        if (self.operand) |op| { // unwraps the u8
+            info("Value: {d}, Grad: {d}, Operand: {c}", .{ self.value, self.grad, op });
+        } else {
+            info("Value: {d}, Grad: {d}", .{ self.value, self.grad });
+        }
     }
 };
 
 pub fn main() void {
-    const a = Node.init(1.0, 0.0);
-    const b = Node.init(2.0, 0.0);
+    const a = Node.init(1.0, 0.0, null);
+
+    const b = Node.init(2.0, 0.0, null);
 
     const c = a.add(b);
 
-    info("Value of a: {d}", .{a.value});
-    info("Value of b: {d}", .{b.value});
-    info("Value of c: {d}", .{c.value});
+    a.print();
+    b.print();
+    c.print();
 }
