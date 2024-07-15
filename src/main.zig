2d80aa594f1e1fccc8a5170c99b9dfa30aebbc2a
Shelwin <91216741+shelwinsunga@users.noreply.github.com>
2024-07-15 16:52:13 +0000

    feat: add dynamic label creation

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,7 @@
 const std = @import("std");
+
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const allocator = gpa.allocator();
 const info = std.log.info;
 
 const Node = struct {
@@ -9,9 +12,10 @@ const Node = struct {
         other: ?*const Node,
     },
     operand: ?u8,
-    label: ?[]const u8,
+    label: []const u8,
 
-    pub fn init(value: f32, grad: f32, children: ?struct { ?*const Node, ?*const Node }, operand: ?u8, label: ?[]const u8) Node {
+    pub fn init(value: f32, grad: f32, children: ?struct { ?*const Node, ?*const Node }, operand: ?u8, label: []const u8) !Node {
+        const duplicateLabel = try allocator.dupe(u8, label);
         return Node{ 
             .value = value, 
             .grad = grad, 
@@ -23,25 +27,23 @@ const Node = struct {
                 .self = null,
                 .other = null,
             }, 
-            .label = label 
+            .label = duplicateLabel
         };
     }
 
-    pub fn add(self: *const Node, other: *const Node) Node {
-        return Node.init(self.value + other.value, 0.0, .{ self, other }, '+', null);
+    pub fn add(self: *const Node, other: *const Node) !Node {
+        const label = try std.fmt.allocPrint(allocator, "{s} + {s}", .{self.label, other.label});
+        return Node.init(self.value + other.value, 0.0, .{ self, other }, '+', label);
     }
 
-    pub fn multiply(self: *const Node, other: *const Node) Node {
-        return Node.init(self.value * other.value, 0.0, .{ self, other }, '*', null);
+    pub fn multiply(self: *const Node, other: *const Node) !Node {
+        const label = try std.fmt.allocPrint(allocator, "{s} * {s}", .{self.label, other.label});
+        return Node.init(self.value * other.value, 0.0, .{ self, other }, '*', label);
     }
 
     pub fn print(self: Node) void {
-        if(self.label) |label| {
-            info("Label: {s}, Value: {d}, Grad: {d}", .{label, self.value, self.grad});
-        } else {
-            info("Value: {d}, Grad: {d}", .{ self.value, self.grad });
-        }
-        
+        info("Label: {s}, Value: {d}, Grad: {d}", .{self.label, self.value, self.grad});
+
         if (self.operand) |op| {
             info("Operand: {c}", .{op});
         }
@@ -53,18 +55,22 @@ const Node = struct {
         if (self.children.other) |child| {
             info("Other: {d}", .{child.value});
         }
-        
+    }
+
+    pub fn deinit(self: *Node) void {
+        allocator.free(self.label);
     }
 };
 
-pub fn main() void {
-    var a = Node.init(5.0, 0.0, null, null, "a");
-    var b = Node.init(2.0, 0.0, null, null, "b");
-    var c = a.add(&b);
-    var d = c.multiply(&a);
+pub fn main() !void {
+    var a = try Node.init(5.0, 0.0, null, null, "a");
+    defer a.deinit();
+    var b = try Node.init(2.0, 0.0, null, null, "b");
+    defer b.deinit();
+    var c = try a.add(&b);
+    defer c.deinit();
 
     a.print();
     b.print();
     c.print();
-    d.print();
 }
