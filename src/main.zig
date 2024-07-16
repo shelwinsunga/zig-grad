40a630ca66b44b6dd659b7671b153626ea610e79
Shelwin <shelwinsunga@gmail.com>
2024-07-15 23:42:38 -0400

    feat: karpathy jupyter notebook example

--- src/main.zig
+++ src/main.zig
@@ -12,59 +12,52 @@ const Node = struct {
         other: ?*const Node,
     },
     operand: ?u8,
-    label: []const u8,
 
-    pub fn init(value: f32, grad: f32, children: ?struct { ?*const Node, ?*const Node }, operand: ?u8, label: []const u8) !Node {
-        const duplicateLabel = try allocator.dupe(u8, label);
+    pub fn init(value: f32, grad: f32, children: ?struct { ?*const Node, ?*const Node }, operand: ?u8) Node {
         return Node{ .value = value, .grad = grad, .operand = operand, .children = if (children) |c| .{
             .self = c[0],
             .other = c[1],
         } else .{
             .self = null,
             .other = null,
-        }, .label = duplicateLabel };
+        } };
     }
 
-    pub fn create(value: f32, label: []const u8) !Node {
-        return Node.init(value, 0.0, null, null, label);
+    pub fn create(value: f32) Node {
+        return Node.init(value, 0.0, null, null);
     }
 
-    pub fn add(self: *const Node, other: *const Node) !Node {
-        const label = try std.fmt.allocPrint(allocator, "{s} + {s}", .{ self.label, other.label });
-        return Node.init(self.value + other.value, 0.0, .{ self, other }, '+', label);
+    pub fn add(self: *const Node, other: *const Node) Node {
+        return Node.init(self.value + other.value, 0.0, .{ self, other }, '+');
     }
 
-    pub fn multiply(self: *const Node, other: *const Node) !Node {
-        const label = try std.fmt.allocPrint(allocator, "{s} * {s}", .{ self.label, other.label });
-        return Node.init(self.value * other.value, 0.0, .{ self, other }, '*', label);
+    pub fn multiply(self: *const Node, other: *const Node) Node {
+        return Node.init(self.value * other.value, 0.0, .{ self, other }, '*');
     }
 
     pub fn print(self: Node) void {
-        info("Label: {s}, Value: {d}, Grad: {d}", .{ self.label, self.value, self.grad });
+        info("Value: {d}, Grad: {d}", .{ self.value, self.grad });
 
         if (self.operand) |op| {
             info("Operand: {c}", .{op});
         }
 
         if (self.children.self) |child| {
-            info("Self: {s}", .{child.label});
+            info("Self: {d}", .{child.value});
         }
 
         if (self.children.other) |child| {
-            info("Other: {s}", .{child.label});
+            info("Other: {d}", .{child.value});
         }
-    }
-
-    pub fn deinit(self: *Node) void {
-        allocator.free(self.label);
     }
 };
 
 pub fn main() !void {
-    var a = try Node.create(3, "a");
-    var b = try Node.create(5, "b");
-    var c = try a.add(&b);
-    var d = try c.add(&b);
+    var a = Node.create(1);
+    var b = Node.create(2);
+    var c = a.multiply(&b);
+    var d = Node.create(1);
+    var e = c.add(&d);
 
-    d.print();
+    e.print();
 }
