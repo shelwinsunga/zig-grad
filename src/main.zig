7a3c46fa2676e93123228c59801b2d4c9ce8d5d0
Shelwin <shelwinsunga@gmail.com>
2024-07-16 00:09:13 -0400

    feat: implement backward on add

--- src/main.zig
+++ src/main.zig
@@ -8,12 +8,12 @@ const Node = struct {
     value: f32,
     grad: f32,
     children: struct {
-        self: ?*const Node,
-        other: ?*const Node,
+        self: ?*Node,
+        other: ?*Node,
     },
     operand: ?u8,
 
-    pub fn init(value: f32, grad: f32, children: ?struct { ?*const Node, ?*const Node }, operand: ?u8) Node {
+    pub fn init(value: f32, grad: f32, children: ?struct { ?*Node, ?*Node }, operand: ?u8) Node {
         return Node{ .value = value, .grad = grad, .operand = operand, .children = if (children) |c| .{
             .self = c[0],
             .other = c[1],
@@ -27,12 +27,26 @@ const Node = struct {
         return Node.init(value, 0.0, null, null);
     }
 
-    pub fn add(self: *const Node, other: *const Node) Node {
+    pub fn add(self: *Node, other: *Node) Node {
         return Node.init(self.value + other.value, 0.0, .{ self, other }, '+');
     }
 
-    pub fn multiply(self: *const Node, other: *const Node) Node {
+    pub fn multiply(self: *Node, other: *Node) Node {
         return Node.init(self.value * other.value, 0.0, .{ self, other }, '*');
+    }
+
+    pub fn backward(self: *Node) void {
+        self.grad = 1;
+        self.addBackward();
+    }
+
+    pub fn addBackward(self: *Node) void {
+        if (self.children.self) |child| {
+            child.grad += self.grad;
+        }
+        if (self.children.other) |child| {
+            child.grad += self.grad;
+        }
     }
 
     pub fn print(self: Node) void {
@@ -59,5 +73,8 @@ pub fn main() !void {
     var d = Node.create(1);
     var e = c.add(&d);
 
+    e.backward();
     e.print();
+    d.print();
+    c.print();
 }
