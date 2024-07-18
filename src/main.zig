184870a14f24e72fcd18902e6c8ff70f864c6c58
Shelwin <91216741+shelwinsunga@users.noreply.github.com>
2024-07-18 21:34:01 +0000

    feat: add neuron

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,9 @@
 const std = @import("std");
 const Value = @import("engine.zig").Value;
+const Neuron = @import("nn.zig").Neuron;
+
+
 
 pub fn main() !void {
-    var a = Value.create(1);
-    var b = Value.create(2);
-    var c = a.multiply(&b);
-    var d = Value.create(1);
-    var e = c.add(&d);
-
-    try e.backward();
-    e.print();
-    b.print();
-    a.print();
+    _ = try Neuron.create(10,true);
 }
--- src/nn.zig
+++ src/nn.zig
@@ -4,7 +4,37 @@ const info = std.log.info;
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
 const ArrayList = std.ArrayList;
+const Value = @import("engine.zig").Value;
+const RndGen = std.rand.DefaultPrng;
 
-pub const Module = struct {
+pub const Neuron = struct {
+    weights: ArrayList(*Value),
+    bias: Value,
+    nonlinear: bool,
+
+    // nin = number of inputs
+    pub fn create(nin: u32, nonlinear: bool) !Neuron {
+        var weights = ArrayList(*Value).init(allocator);
+        var rnd = RndGen.init(0);
+
+        for (0..nin) | i | {
+            var weight = Value.create(randomWeight(&rnd, -1, 1));
+            try weights.append(&weight);
+            info("Weight {d}: {d}", .{ i, weight.value });
+        }
+
+        const bias = Value.create(0);
+        // info("Bias: {d}", .{bias.value});
+
+        return Neuron{
+            .weights = weights,
+            .bias = bias,
+            .nonlinear = nonlinear
+        };
+    }
     
+    fn randomWeight(rnd: *RndGen, a: f32, b: f32) f32 {
+        return a + (b - a) * rnd.random().float(f32);
+    }
+
 };
